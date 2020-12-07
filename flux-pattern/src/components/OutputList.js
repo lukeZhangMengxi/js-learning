7a791894ce4a4ad0164ab742dd6e760ddf0dd6bf
Mengxi <zmx94824@gmail.com>
2020-12-07 11:59:47 -0800

    Delete button for each elements (#3)

--- flux-pattern/src/components/OutputList.js
+++ flux-pattern/src/components/OutputList.js
@@ -2,11 +2,20 @@ import React, { Component, PropTypes } from 'react'
 import OutputListElement from './OutputListElement.js'
 
 export default class OutputList extends Component {
+
+  deleteElement(uuid) {
+    console.log("Deleting uuid of " + uuid);
+    let removeIndex = this.props.elementList.map(item => item.uuid).indexOf(uuid);
+    this.props.elementList.splice(removeIndex, 1);
+    this.forceUpdate();
+  }
+
   render() {
     return (
       <ul>
         {this.props.elementList.map(element =>
           <OutputListElement
+            deleteCallback={this.deleteElement.bind(this)}
             key={element.id}
             {...element}
           />
--- flux-pattern/src/components/OutputListElement.js
+++ flux-pattern/src/components/OutputListElement.js
@@ -1,10 +1,13 @@
 import React, { Component, PropTypes } from 'react'
 
 export default class OutputListElement extends Component {
+
   render() {
     return (
       <li>
-        {this.props.id} : {this.props.text}
+        {this.props.uuid} : {this.props.text} <button onClick={() => {
+          this.props.deleteCallback(this.props.uuid);
+        }}>Delete</button>
       </li>
     )
   }
